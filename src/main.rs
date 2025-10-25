b150f88893778c4b899d44af9ca6ebdd94254b19
John Tobin <johntobin@johntobin.ie>
2025-10-25 22:35:21 +0100

    feat: implement `lock_file`

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 use clap::Parser;
+use fs4::fs_std::FileExt;
+use std::fs::File;
+use std::path::Path;
+use std::time::{Duration, Instant};
 
 /// A program that wraps a command, and ensures that only one instance of the
 /// command is running at a time.
@@ -24,6 +28,30 @@ struct Args {
     /// The command to run.
     #[arg(trailing_var_arg = true, required = true)]
     command: Vec<String>,
+}
+
+#[allow(dead_code)]
+fn lock_file(lock_filename: &Path, lock_timeout: Duration) -> Result<File, String> {
+    let start = Instant::now();
+    loop {
+        let file = File::create(lock_filename).map_err(|e| e.to_string())?;
+        match file.try_lock_exclusive() {
+            Ok(true) => return Ok(file),
+            Ok(false) => {
+                if start.elapsed() > lock_timeout {
+                    return Err(format!(
+                        "Timeout waiting for lockfile after {lock_timeout:?} seconds"
+                    )
+                    .to_string());
+                }
+                std::thread::sleep(Duration::from_millis(100));
+            }
+            // TODO: how do I test this?
+            Err(error_message) => {
+                return Err(error_message.to_string());
+            }
+        }
+    }
 }
 
 fn realmain(args: Args) {
@@ -53,5 +81,26 @@ mod realmain {
             "echo",
             "foo",
         ]));
+    }
+}
+
+#[cfg(test)]
+mod lock_file {
+    use super::*;
+    use std::env;
+    use std::thread;
+
+    #[test]
+    fn test_lock_file() {
+        let mut temp_file = env::temp_dir();
+        temp_file.push("test.lock");
+
+        let _lock = lock_file(&temp_file, Duration::from_secs(1)).unwrap();
+
+        let lock_result = thread::spawn(move || lock_file(&temp_file, Duration::from_secs(1)))
+            .join()
+            .unwrap();
+
+        assert!(lock_result.is_err());
     }
 }
